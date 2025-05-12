147b163b6bf3e4ef269dbea2f1111659817cb204
samuel <samawyck@gmail.com>
2025-05-12 13:52:41 -0500

    Create hoomepage hero section

---
+++ src/components/homepage.jsx
@@ -0,0 +1,21 @@
+import "../styles/homePage.css";
+
+
+function HomePage() {
+
+    return (
+    <main className="homepage-main">
+        <section className="hero-section">
+            <div className="hero-img"></div>
+            <div className="banner-text">
+                <p>shopBay</p>
+                <p>- shop with confidence</p>
+            </div>
+        </section>
+    </main>
+    );
+};
+
+
+
+export default HomePage;
