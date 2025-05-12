0fbbb7b7e504cfffdb8092e50da258b97256e686
samuel <samawyck@gmail.com>
2025-05-12 12:40:56 -0500

    Create counter

---
+++ src/components/counter.jsx
@@ -0,0 +1,39 @@
+import { Component } from "react";
+import "../styles/counter.css";
+
+
+class Counter extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            count: this.props.count,
+        };
+
+        this.update = this.update.bind(this);
+    };
+
+    update(change) {
+        this.setState(function(state) {
+            return {...state, count: state.count + change}
+        });
+    };
+
+    render() {
+        let content = this.state.count;
+        const plusSpan = <span className="plusSpan">+</span>
+
+        if (content === 0) {
+            return null;
+        }
+
+        if (content > 9) {
+            content = 9;
+        }
+
+        return <p className="counter">{content}{content < 9 || plusSpan}</p>
+    };
+};
+
+
+export default Counter;
