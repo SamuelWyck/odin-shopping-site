08fb4d4ac2a45834c381f896936bd65e3111b753
samuel <samawyck@gmail.com>
2025-05-12 14:38:52 -0500

    Add mission statement section

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -9,8 +9,15 @@ function HomePage() {
             <div className="hero-img"></div>
             <div className="banner-text">
                 <p>shopBay</p>
-                <p>- shop with confidence</p>
+                <p>shop with confidence</p>
             </div>
+        </section>
+        <section className="statement-section">
+            <p>Mission Statement</p>
+            <p>Our passion is to offer quality products at an affordable price while using minimal child labor. We do this by partnering with ethical suppliers like AfricanWarlordOutlet.</p>
+        </section>
+        <section className="top-products-section">
+            <p>Top Products</p>
         </section>
     </main>
     );
