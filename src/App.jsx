ccc27c0d5ac23ea0c0d8087d8a5152883dce829b
samuel <samawyck@gmail.com>
2025-05-12 11:18:03 -0500

    Install header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
+import Header from './components/header.jsx';
+import { Link } from 'react-router-dom';
+import cartImg from "./assets/cart.svg";
 
 
 function App() {
 
-
     return (
         <>
-        
+        <Header>
+            <Link to="/">Home</Link>
+            <Link to="shop">Shop</Link>
+            <Link to="cart">
+            <img src={cartImg} alt="cart"/>
+            </Link>
+        </Header>
         </>
-    )
+    );
 };
 
 
