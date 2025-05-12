eb965f6b9dd071997bf32c9a0254feb91c26c8f9
samuel <samawyck@gmail.com>
2025-05-12 12:42:56 -0500

    Install counter and fetch products

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css';
 import Header from './components/header.jsx';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import cartImg from "./assets/cart.svg";
+import { createRef, useEffect, useRef, useState } from 'react';
+import Cart from './cart.js';
+import Counter from './components/counter.jsx';
+import APIManager from './APIManager.js';
 
 
 function App() {
+    const cart = useRef(new Cart);
+    const [products, setProducts] = useState(null);
+
+    useEffect(function() {
+        const api = new APIManager();
+        api.getProducts().then(function(res) {
+            setProducts(res);
+        });
+    }, []);
+    
+    const counterRef = createRef();
+    const counter = <Counter ref={counterRef} count={cart.current.getItemCount()} />
 
     return (
         <>
@@ -13,8 +29,10 @@ function App() {
             <Link to="shop">Shop</Link>
             <Link to="cart">
             <img src={cartImg} alt="cart"/>
+            {counter}
             </Link>
         </Header>
+        <Outlet />
         </>
     );
 };
