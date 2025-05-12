92d30941bc1ecc1fe22744c1d96b7d3873b1bf9c
samuel <samawyck@gmail.com>
2025-05-12 16:08:26 -0500

    Add currency formatter nad remove unused lines

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -4,8 +4,12 @@ import "../styles/productCard.css";
 function ProductCard({data, counterRef, cartRef}) {
     const title = data.title;
     const price = data.price;
-    const description = data.description;
     const image = data.image;
+
+    const formatter = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    });
 
 
     function clickHandler() {
@@ -20,8 +24,7 @@ function ProductCard({data, counterRef, cartRef}) {
             <img src={image} alt={title} />
         </div>
         <p className="product-title">{title}</p>
-        {/* <p className="product-description">{description}</p> */}
-        <p className="product-price">{`$${price}`}</p>
+        <p className="product-price">{`${formatter.format(price)}`}</p>
         <button onClick={clickHandler}>Add to Cart</button>
     </div>
     );
