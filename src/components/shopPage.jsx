51f4e07d03e99be594b5d64ad4f34eecb9ef3e0c
samuel <samawyck@gmail.com>
2025-05-13 09:14:50 -0500

    Create shopPage

---
+++ src/components/shopPage.jsx
@@ -0,0 +1,39 @@
+import { useOutletContext } from "react-router-dom";
+import { useState } from "react";
+import ProductCard from "./productCard.jsx";
+import "../styles/shopPage.css";
+
+
+function ShopPage() {
+    const context = useOutletContext();
+    const [products, setProducts] = useState(createProductList(context));
+
+
+    function createProductList(context) {
+        const productElements = [];
+        for (let productData of context.products) {
+            console.log(productData)
+            const productCard = <ProductCard 
+                data={productData}
+                counterRef={context.counterRef}
+                cartRef={context.cartRef}
+                key={productData.id}
+            />
+            productElements.push(productCard);
+        }
+        return productElements;
+    };
+
+
+    return (
+    <main className="shopping-main">
+        <p className="shopping-title">Product Line</p>
+        <div className="shopping-products-list">
+            {products}
+        </div>
+    </main>
+    );
+};
+
+
+export default ShopPage;
